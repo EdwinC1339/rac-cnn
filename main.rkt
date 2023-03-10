e06e9ff01118b7378431f3a06184df11ce96806c
EdwinC1339 <edwin.camuy@upr.edu>
2023-03-10 14:45:03 -0400

    Dummy DLL and racket interface

--- main.rkt
+++ main.rkt
@@ -1,7 +1,20 @@
-#lang racket
+#lang racket/base
+(require ffi/unsafe
+         ffi/unsafe/define)
+ 
+(define-ffi-definer define-cnn (ffi-lib ".\\libCNN\\x64\\Debug\\libCNN"))
 
-(define reals (inclusive-range -2 2 (/ 1 2)))
-(define imags (map (lambda (x) (* x 0+i)) reals))
+(define-cnn fibonacci_init (_fun _int _int -> _void))
+(define-cnn fibonacci_next (_fun -> _bool))
+(define-cnn fibonacci_current (_fun -> _int))
 
-(define plane (map (lambda (p) (apply + p)) (cartesian-product reals imags)))
-(display plane)
+(define (init) (fibonacci_init 1 1))
+(define (next) (fibonacci_next))
+(define (current) (fibonacci_current))
+
+(void (init))
+
+(define (fib n) (cond
+    ((= n 0) (list (current)))
+    (else (cons (car (cons (current) (next))) (fib (- n 1))))
+))
